b6ab07a6e3390a732c76dbe9c9734bf62d4529e2
shuhui <793983383@qq.com>
2019-05-14 12:45:28 +0800

    geg
    gge

--- Example/Pods/Target Support Files/Pods-YSHKit_Example/Pods-YSHKit_Example-frameworks.sh
+++ Example/Pods/Target Support Files/Pods-YSHKit_Example/Pods-YSHKit_Example-frameworks.sh
@@ -155,20 +155,14 @@ strip_invalid_archs() {
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Masonry/Masonry.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SDWebImage/SDWebImage.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/TTTAttributedLabel/TTTAttributedLabel.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/YSHKit/YSHKit.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/YSHVersionHelper/YSHVersionHelper.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/YYKit/YYKit.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/pop/pop.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Masonry/Masonry.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SDWebImage/SDWebImage.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/TTTAttributedLabel/TTTAttributedLabel.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/YSHKit/YSHKit.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/YSHVersionHelper/YSHVersionHelper.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/YYKit/YYKit.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/pop/pop.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
--- Example/Pods/Target Support Files/YSHVersionHelper/YSHVersionHelper-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_YSHVersionHelper : NSObject
-@end
-@implementation PodsDummy_YSHVersionHelper
-@end
